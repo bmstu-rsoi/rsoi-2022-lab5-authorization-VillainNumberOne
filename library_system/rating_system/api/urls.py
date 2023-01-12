312a4ee0fa50916579f2ec56bd350c8afc59384f
VillainNumberOne <66220826+VillainNumberOne@users.noreply.github.com>
2023-01-12 10:21:54 +0300

    fix username urlpattern

--- library_system/rating_system/api/urls.py
+++ library_system/rating_system/api/urls.py
@@ -3,5 +3,5 @@ from api import views
 
 urlpatterns = [
     re_path(r'^api/v1/ratings$', views.rating_system_api),
-    re_path(r'^api/v1/ratings/(?P<username>[A-Za-z0-9_ ]+)$', views.rating_system_api),
+    re_path(r'^api/v1/ratings/(?P<username>[A-Za-z0-9_ .@]+)$', views.rating_system_api),
 ]
